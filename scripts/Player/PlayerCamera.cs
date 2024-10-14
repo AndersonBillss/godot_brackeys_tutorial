0801e16a07d54bdde077a4435674840d94c1b7a7
AndersonBillss <anderson@thebillsfamily.com>
2024-10-13 22:58:33 -0600

    made player camera script work and added a coin script

--- scripts/Player/PlayerCamera.cs
+++ scripts/Player/PlayerCamera.cs
@@ -4,9 +4,11 @@ using System;
 public partial class PlayerCamera : Camera2D
 {
     private Node2D parentNode;
+    private Vector2 prevParentPosition;
     public override void _Ready()
     {
         parentNode = GetParent() as Node2D;
+        prevParentPosition = parentNode.GlobalPosition;
         if (parentNode == null)  // Make sure the parent node exists
         {
             GD.PushError("The node with the PlayerFollow script must have a parent node");
@@ -14,8 +16,15 @@ public partial class PlayerCamera : Camera2D
     }
     public override void _PhysicsProcess(double delta)
 	{
-        Vector2 position = GlobalPosition;
+        Vector2 newPosition = Position;
         Vector2 parentPosition = parentNode.GlobalPosition;
+        Vector2 parentVelocity = parentPosition - prevParentPosition;
+        prevParentPosition = parentPosition;
+
+        newPosition-=parentVelocity;
+        newPosition/=(float)1.1;
+
+        Position = newPosition;
 	}
 
 }
